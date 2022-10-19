1390052499845240c6cbdb1485dc2c57a53f7681
Ananda Ramakrishna <ramakrishnaka99@gmail.com>
2022-10-19 09:35:05 +0530

    this commiting on 19-oct-2022

--- src/A8_Program.java
+++ src/A8_Program.java
@@ -1,3 +1,5 @@
+import java.awt.datatransfer.FlavorEvent;
+
 // converting string to integers , float , double..
 public class A8_Program {
 
@@ -11,6 +13,16 @@ public class A8_Program {
 		int b = Integer.parseInt(intvalue1);
 		System.out.println("Adding 2 number after converting string to integer --");
 		System.out.println(a+b);
+		System.out.println("=================This is floating values conversion ----------------------");
+		String floatvalue ="10.20";
+		String floatvalue1="2.04";
+		System.out.println("Before conversion floating values adding ----------");
+		System.out.println(floatvalue+floatvalue1);
+		float c = Float.parseFloat(floatvalue);
+		float d = Float.parseFloat(floatvalue1);
+		System.out.println("After conversion adding the floading values -------------");
+		System.out.println(c+d +" ---this is after conversion");
+		
 		
 	}
 }
