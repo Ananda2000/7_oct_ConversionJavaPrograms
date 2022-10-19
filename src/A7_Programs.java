49f112f2684630742541eb7fe43a9d11d8632ae9
Ananda Ramakrishna <ramakrishnaka99@gmail.com>
2022-10-19 08:39:35 +0530

    comminting on 19 oct 2022

--- src/A7_Programs.java
+++ src/A7_Programs.java
@@ -1,4 +1,4 @@
-// Use of valueof() method in java
+// Use of valueof() method in java used to convert integer to string..
 public class A7_Programs 
 {
 	public static void main(String [] args)
@@ -6,7 +6,9 @@ public class A7_Programs
 		int a =10;
 		System.out.println(a+10);
 		String intString = String.valueOf(a);
+		System.out.println("Converted interger to string ---");
 		System.out.println(intString+"20"+intString);
+		
 	}
 
 }
---
+++ src/A8_Program.java
@@ -0,0 +1,16 @@
+// converting string to integers , float , double..
+public class A8_Program {
+
+	public static void main (String[] args)
+	{
+		System.out.println("Below are the string values");
+		String intvalue = "10";
+		String intvalue1 ="20";
+		System.out.println(intvalue+intvalue1);
+		int a = Integer.parseInt(intvalue);
+		int b = Integer.parseInt(intvalue1);
+		System.out.println("Adding 2 number after converting string to integer --");
+		System.out.println(a+b);
+		
+	}
+}
