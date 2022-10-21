0c3d936f8f41e35814ca0dd4305f5a65e20abe82
Ananda Ramakrishna <ramakrishnaka99@gmail.com>
2022-10-21 08:30:43 +0530

    committing on 21-oct-2022

---
+++ src/A9_program.java
@@ -0,0 +1,24 @@
+import supportingClasses.Student;
+
+//" object creation and iterate through object."
+public class A9_program 
+{
+	public static void main (String[] args)
+	{
+		Student s1= new Student();
+		
+		s1.setAge(25);
+		s1.setName("Ananda");
+		s1.setRollno("989AK");
+		
+		Student s2 = new Student();
+		s2.setAge(30);
+		s2.setName("soma N.T");
+		s2.setRollno("8965YUJ");
+		
+		
+		
+		
+	}
+
+}
---
+++ src/supportingClasses/Student.java
@@ -0,0 +1,30 @@
+// this class is created as supporting class for A9_program in default package...
+
+package supportingClasses;
+
+public class Student
+{
+	private String name;
+	private int age;
+	private String rollno;
+	public String getName() {
+		return name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getRollno() {
+		return rollno;
+	}
+	public void setRollno(String rollno) {
+		this.rollno = rollno;
+	}
+	
+
+}
